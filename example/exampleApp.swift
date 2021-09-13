370e1f71bf908bfd5936060ca25196792bbaaad3
Aravind Chowdary <kamaravichow@gmail.com>
2021-09-13 17:45:30 +0530

    Initial Commit

---
+++ example/exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  exampleApp.swift
+//  example
+//
+//  Created by Aravind Chowdary Kamani on 13/09/21.
+//
+
+import SwiftUI
+
+@main
+struct exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
