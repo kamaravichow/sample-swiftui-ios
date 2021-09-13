fdfbc55da68d1583850da97f66c96aae7e5acde4
Aravind Chowdary <kamaravichow@gmail.com>
2021-09-13 17:49:56 +0530

    profile model class made

---
+++ example/Profile.swift
@@ -0,0 +1,13 @@
+//
+//  Profile.swift
+//  example
+//
+//  Created by Aravind Chowdary Kamani on 13/09/21.
+//
+
+struct Profile {
+    let username : String
+    let full_name : String
+    
+    
+}
